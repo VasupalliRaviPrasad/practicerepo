1c51ace20042ed3ce3e9825f32f79db6a1d00ed3
Vasupalli <vasupalliravi13@gmail.com>
2025-03-28 09:45:56 +0530

    made changes in java8 in feature branch

--- src/com/functions/Person.java
+++ src/com/functions/Person.java
@@ -4,10 +4,12 @@ public class Person {
 	
 	String name;
 	int age;
+	String address;
 	
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
+		this.address = address;
 	}
 
 }
